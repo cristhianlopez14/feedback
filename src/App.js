beb48f44a99d7cf724e0142fdf0d749350bb5edf
Your Name <you@example.com>
2022-11-15 08:59:25 -0500

    primer commit feedback

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () =>{
+
+  const bootcamps=[
+  {
+    id:1,
+    name:'PHP Bootcamp',
+    average_rating:8
+  },
+  {
+    id:2,
+    name:'Javascript Bootcamp',
+    average_rating:10
+  }
+]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h1>Listado de Bootcamps</h1>
+    
+    <ul>
+      { bootcamps.map((bootcamp, index) => (
+        <li key={index}> { bootcamp.name } </li>
+      )) }
+    </ul>
+  </div>
   );
 }
 
